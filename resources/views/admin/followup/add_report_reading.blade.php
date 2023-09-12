7b085759679be17574e7beadd7ec22cfc6e338a8
AhmedNabil1994 <94ahmed.nabil@gmail.com>
2023-09-12 19:18:55 +0300

    Follow up words spelling modifications

--- resources/views/admin/followup/add_report_reading.blade.php
+++ resources/views/admin/followup/add_report_reading.blade.php
@@ -31,10 +31,10 @@
                             <h2>{{ trans('website.add_student') }}</h2>
 
                         </div> <div class="item-title d-flex justify-content-center mx-4">
-                            <a href="{{route('admin.followup.index')}}" class="icon"><i class="fa fa-paper-plane mx-3">خطة متابعه المعلمين</i></a>
-                            <a href="{{route('admin.followup.quran')}}" class="icon"><i class="fa fa-paper-plane mx-3">متابعه القراءن</i></a>
-                            <a href="{{route('admin.followup.create')}}" class="icon"><i class="fa fa-paper-plane mx-3">متابعه حصة دراسية</i></a>
-                            <a href="{{route('admin.followup.reading')}}" class="icon"><i class="fa fa-paper-plane mx-3">متابعه القراءة</i></a>
+                            <a href="{{route('admin.followup.index')}}" class="icon"><i class="fa fa-paper-plane mx-3">خطة متابعة المعلمين</i></a>
+                            <a href="{{route('admin.followup.quran')}}" class="icon"><i class="fa fa-paper-plane mx-3">متابعة القرآن</i></a>
+                            <a href="{{route('admin.followup.create')}}" class="icon"><i class="fa fa-paper-plane mx-3">متابعة حصة دراسية</i></a>
+                            <a href="{{route('admin.followup.reading')}}" class="icon"><i class="fa fa-paper-plane mx-3">متابعة القراءة</i></a>
                         </div>
                         <form action="{{route('admin.followup.storeReading')}}" method="post" class="form-horizontal" enctype="multipart/form-data">
                             @csrf
